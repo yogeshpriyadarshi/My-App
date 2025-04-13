d6c6f4415ad13e31ca9875647a4f4d504d0f5fa9
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-04-13 19:51:42 +0530

    add hello world

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <><p>hello wold</p>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
