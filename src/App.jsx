a53637836cd73e1bb03800342426c996f6625afb
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-04-14 20:53:08 +0530

    add Home and learn useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -2,33 +2,13 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './Home'
 
 function App() {
   const [count, setCount] = useState(2)
 
   return (
-    <><p>hello wold</p>
-      <div>
-        <a href="https://www.google.co.in/" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h4>Vite + React stdudentjs</h4>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 2)}>
-          counts are {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Home/>
   )
 }
 
