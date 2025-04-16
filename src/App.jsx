97c26267bf64490b16c9b6307947c9d4455e6285
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-04-16 08:06:23 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './Home'
 
 function App() {
-  const [count, setCount] = useState(2)
+  const [count, setCount] = useState(100)
+  const [co, setCo] = useState(1)
+  
+  useEffect( ()=> { 
+    // setCount(count-10)
+    console.log("useEffect worked")},[count])
+ function Test(){
+console.log("test function call")
 
-  return (
-    <Home/>
+ }
+ Test()
+  return(
+  
+<div>
+  <p>  The count is {count}.</p>
+  <button onClick={()=> setCount(count -2)} > click here 
+</button>
+<button onClick={()=> setCo(co -2)} > click here 
+</button>
+<Home/>
+</div>
   )
 }
 
