e750b3eb7135ee97f499e24cd53bb9f81adc3fea
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-04-18 16:59:03 +0530

    update

--- src/CreateAccount.jsx
+++ src/CreateAccount.jsx
@@ -4,18 +4,25 @@ import Navbar from './Navbar'
 export default function CreateAccount() {
 const [name, setName] =useState("");
 const [age, setAge] =useState("");
+const [contact, setContact] =useState("");
+const [fullname, setFullname] = useState("");
+const [email, setEmail] = useState("");
 
-const handleSubmit =(event)=> {
-    event.Prevent.Default();
+const handleSubmit =()=> {
+    //  e.Prevent.Default();
     console.log("name:", name);
     console.log("Age:", age);
+    setFullname(name);
 };
-  return (
+
+  return(
     <>
     <Navbar/>
     <div>
         <h1>Create Free Account! </h1>
-        <form onSubmit={handleSubmit} >
+        <h3> hello {fullname} </h3>
+        {/* <form > */}
+          {/* onSubmit={handleSubmit} > */}
           <div>
           <label >Name: </label> <br/>
           <input type="text" 
@@ -30,9 +37,21 @@ const handleSubmit =(event)=> {
           onChange={(e)=> setAge(e.target.value)}
           placeholder="Age" />
           </div>
-          <br/>
-          <button type="submit"> Sumbit </button>
-        </form>
+          <div>
+            <label>Contact Number:</label> <br/>
+            <input type="number" 
+                   placeholder='Contact Number'
+                   value={contact}
+                   onChange={(e)=> setContact(e.target.value) }
+            />
+          </div>
+          <div>
+            <label>Eamail:</label> <br/>
+            <input type="email" placeholder="Enter valid Email" value={email} 
+            onChange={(e)=>setEmail(e.target.value) } />
+          </div>
+          <button onClick={handleSubmit}> click me </button>
+        {/* </form> */}
     </div>
     </>
    
