9d5b91a5a0856dc5abe4406ff372394ac61b8e73
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-04-27 23:04:06 +0530

    change

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,67 +1,71 @@
-import { useContext, useEffect,useState } from 'react'
-import axios from 'axios';
-import { Link, useNavigate } from 'react-router-dom'
-import {AuthContext} from './App'
+import { useContext, useEffect, useState } from "react";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { AuthContext } from "./App";
 
-import  './Login.css';
+import "./Login.css";
 
 export default function Login() {
-  const navigate= useNavigate();
- const {state,dispatch}= useContext(AuthContext)
- const [email, setEmail] = useState("");
- const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const { state, dispatch } = useContext(AuthContext);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
+  async function ClickHandler() {
+    console.log(email);
+    console.log(password);
 
-async function ClickHandler(){
+    const res = await axios.post("http://localhost:5000/login", {
+      email,
+      password,
+    });
 
-
-  console.log(email)
-  console.log(password)
-
-  const res = await axios.post("http://localhost:5000/login",{email,password});
-  // const res={data:{
-  //   message:"Login Succefully",
-  //   success:true,
-  //   user:{
-  //     name:"piyush",
-  //     email:"Piyushpriyadarshi329@gmail.com",
-  //     mobile:"987786876"
-  //   }
-  // }}
-
- if(res.data.success){
- setEmail("");
- setPassword("");
- dispatch({type:"LOGIN",user:res.data.user})
- }else{
-  alert(res.data.message);
- }
-}
+    if (res.data.success) {
+      setEmail("");
+      setPassword("");
+      dispatch({ type: "LOGIN", user: res.data.user });
+    } else {
+      alert(res.data.message);
+    }
+  }
 
   return (
     <>
-      <div id="login_c"> 
-        <div>     
-        <Link className="tab" to="/login"><button className="id_button" > login </button></Link>
-        <Link className="tab" to="/createAccount"><button className="id_button">create account</button></Link>
-          </div>    
-     <div id="login_a">
-        <h1>Login</h1>   <br/>
-       <p className ="Login_class" > Email </p>   
-      <input className='login_in' type="email" value={email} placeholder='Enter Email' 
-      onChange={(e)=> setEmail(e.target.value)}   /> 
-      <br/>       <br/>
-       <p className ="Login_class"  > Password </p>   
-      <input className='login_in' type="password" placeholder="password"
-      onChange={(e)=>setPassword(e.target.value)} />   <br/>  <br/>
-      <br/>
-
-       <button id="login_b" type="submit" onClick={()=> ClickHandler() } > Log in </button> 
-
-
+      <div id="login_c">
+        <div>
+          <Link className="tab" to="/login">
+            <button className="id_button"> login </button>
+          </Link>
+          <Link className="tab" to="/createAccount">
+            <button className="id_button">create account</button>
+          </Link>
+        </div>
+        <div id="login_a">
+          <h1>Login</h1> <br />
+          <p className="Login_class"> Email </p>
+          <input
+            className="login_in"
+            type="email"
+            value={email}
+            placeholder="Enter Email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br /> <br />
+          <p className="Login_class"> Password </p>
+          <input
+            className="login_in"
+            type="password"
+            placeholder="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />{" "}
+          <br /> <br />
+          <br />
+          <button id="login_b" type="submit" onClick={() => ClickHandler()}>
+            {" "}
+            Log in{" "}
+          </button>
+        </div>
       </div>
-     </div>
-    
     </>
-  )
-  }
+  );
+}
