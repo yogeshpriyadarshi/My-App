91d994940184554700bb395c437d241b84970c2e
yogesh priyadarshi <yogeshpriyadarshi55@gmail.com>
2025-05-06 09:33:28 +0530

    target

--- src/MyCalendar.jsx
+++ src/MyCalendar.jsx
@@ -1,49 +1,51 @@
-import React from 'react'
+import React from "react";
 import { useState, createContext, useContext, useEffect } from "react";
 import Calendar from "react-calendar";
 import moment from "moment";
 import { AuthContext } from "./App";
 
 import { FaRegCalendarCheck } from "react-icons/fa";
-import 'react-calendar/dist/Calendar.css';
-import './App';
-import './MyCalendar.css';
- 
+import "react-calendar/dist/Calendar.css";
+import "./App";
+import "./MyCalendar.css";
+
 function MyCalendar() {
-    const { state, dispatch } = useContext(AuthContext);
+  const { state, dispatch } = useContext(AuthContext);
   const [value, setValue] = useState(moment(new Date()).format("YYYY-MM-DD"));
   const [isOpen, setIsOpen] = useState(false);
 
-  function dateHandler(event){
-    setValue( moment(event).format("YYYY-MM-DD") );
+  function dateHandler(event) {
+    setValue(moment(event).format("YYYY-MM-DD"));
     console.log("date after selction dateHandler", value);
     setIsOpen(false);
-      }
-useEffect(
-()=>{
-
-  const Date = { date: value}  
-   dispatch( {type: "UPDATEDATE", date:Date})
-},[value]);
-
+  }
+  useEffect(() => {
+    const Date = { date: value };
+    dispatch({ type: "UPDATEDATE", date: Date });
+  }, [value]);
 
   return (
-    <>  
-    <div>  
+    <>
+      <div>
+        <button onClick={() => setIsOpen(true)}>
+          {" "}
+          <FaRegCalendarCheck size={40} color="blue" />
+          {value}{" "}
+        </button>
 
-    <button  onClick={()=> setIsOpen(true)}> <FaRegCalendarCheck size={40} color="blue" />
-           {value}      </button>
-    
-          {isOpen && (
-            <>
-               <div id="calendar_ida">
-                <div id="calendar_idb">
-                 <Calendar onChange={(event)=> dateHandler(event)} value={value} /> 
-                </div>
-              </div> 
-            </>
-          )}
-    </div>
+        {isOpen && (
+          <>
+            <div id="calendar_ida">
+              <div id="calendar_idb">
+                <Calendar
+                  onChange={(event) => dateHandler(event)}
+                  value={value}
+                />
+              </div>
+            </div>
+          </>
+        )}
+      </div>
     </>
   );
 }
--- src/Target.jsx
+++ src/Target.jsx
@@ -8,17 +8,35 @@ import moment from "moment";
 
 export default function Target() {
   const { state, dispatch } = useContext(AuthContext);
- const [type, setType] = useState( []);
- const [ currentTarget, setCurrentTarget]=useState(" ");
- const [uploadStatus, setUploadStatus] = useState(
-  {email:state.email, name:" ", date:" ", status:" " });
-  const [status, setStatus]= useState([]);
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  const [type, setType] = useState([]);
+  const [currentTarget, setCurrentTarget] = useState(" ");
+  const [uploadStatus, setUploadStatus] = useState({
+    email: state.email,
+    name: " ",
+    date: " ",
+    status: " ",
+  });
+  const [status, setStatus] = useState([]);
   const [aim, setAim] = useState({
     email: state.email,
     type: " ",
     customName: " ",
-    year: 2025,
-    month: "may",
+    year: new Date().getFullYear(),
+    month: monthNames[new Date().getMonth()],
     firstDate: 0,
     lastDate: 0,
     setTarget: " ",
@@ -28,39 +46,61 @@ export default function Target() {
   }
   async function submitHandler(e) {
     e.preventDefault();
-    console.log("print aim from fronted to backend",aim);
+    let payload = aim;
+
+    if (aim.type === "Yearly") {
+      payload.firstDate = `${aim.year}-01-01`;
+      payload.lastDate = `${aim.year}-12-31`;
+    }
+    console.log(
+      " first date ",
+      payload.firstDate,
+      "last date",
+      payload.lastDate
+    );
+    console.log("print aim from fronted to backend", payload);
+
     const res = await axios.post("http://localhost:5000/setTarget", aim);
   }
 
-async function statusHandle(e){
-  // const ida = e.target.value;
-  // const typea = type.find( type => type.id===ida)
-  // console.log("after selection of target name",ida);
-  setCurrentTarget(e.target.value);
-  setUploadStatus({...uploadStatus, name:e.target.value })
-  const status= { email: state.email, name:e.target.value}
-  console.log("status Handle",status);
-  const res = await axios.post("http://localhost:5000/statusTarget", status ); 
-  console.log("setStatus",res.data);
-  setStatus(res.data);
-};
- async function uploadStatusHandler(e){
-  e.preventDefault();
-console.log("upload data", uploadStatus);
-const res = await axios.post("http://localhost:5000/uploadstatus",uploadStatus);
-console.log("show status of target",res);
-}
+  async function statusHandle(e) {
+    // const ida = e.target.value;
+    // const typea = type.find( type => type.id===ida)
+    // console.log("after selection of target name",ida);
+    setCurrentTarget(e.target.value);
+    setUploadStatus({ ...uploadStatus, name: e.target.value });
+    const status = { email: state.email, name: e.target.value };
+    console.log("status Handle", status);
+    const res = await axios.post("http://localhost:5000/statusTarget", status);
+    console.log("setStatus", res.data);
+    setStatus(res.data);
+  }
+  async function uploadStatusHandler(e) {
+    e.preventDefault();
+    console.log("upload data", uploadStatus);
+    const res = await axios.post(
+      "http://localhost:5000/uploadstatus",
+      uploadStatus
+    );
+    console.log("show status of target", res);
+  }
 
-
-
-
-async function customTarget(e) {
-  const customa = {email: state.email, type:e.target.value};
-console.log("input custom",state.email, e.target.value,"why not custom",customa);
-  const resType =await axios.post("http://localhost:5000/customTarget",customa);
-  console.log("backend date of custom",resType);
-  setType(resType.data);
-}
+  async function customTarget(e) {
+    const customa = { email: state.email, type: e.target.value };
+    console.log(
+      "input custom",
+      state.email,
+      e.target.value,
+      "why not custom",
+      customa
+    );
+    const resType = await axios.post(
+      "http://localhost:5000/customTarget",
+      customa
+    );
+    console.log("backend date of custom", resType);
+    setType(resType.data);
+  }
   return (
     <>
       <Navbar />
@@ -82,7 +122,10 @@ console.log("input custom",state.email, e.target.value,"why not custom",customa)
 
           {aim.type === "Custom" && (
             <div>
-              <label> Target Name(Give different name from previous one) </label>
+              <label>
+                {" "}
+                Target Name(Give different name from previous one){" "}
+              </label>
               <input
                 type="text"
                 value={aim.customName}
@@ -92,17 +135,30 @@ console.log("input custom",state.email, e.target.value,"why not custom",customa)
               />
               <br />
               <label> Starting date:</label>
-              <input type="date"  onChange={(e)=>setAim({...aim, firstDate:moment(e.target.value).format("YYYY-MM-DD")})} />
+              <input
+                type="date"
+                onChange={(e) =>
+                  setAim({
+                    ...aim,
+                    firstDate: moment(e.target.value).format("YYYY-MM-DD"),
+                  })
+                }
+              />
               <br />
 
               <label> Ending date:</label>
-              <input type="date" onChange={(e)=>setAim({...aim, lastDate:e.target.value})} />
+              <input
+                type="date"
+                onChange={(e) => setAim({ ...aim, lastDate: e.target.value })}
+              />
               <br />
-              
-              <label>  Describe  Target </label>
-              <input type="text" onChange={(e)=>setAim({...aim, setTarget:e.target.value})}  />
-              <br/>
 
+              <label> Describe Target </label>
+              <input
+                type="text"
+                onChange={(e) => setAim({ ...aim, setTarget: e.target.value })}
+              />
+              <br />
             </div>
           )}
 
@@ -173,71 +229,77 @@ console.log("input custom",state.email, e.target.value,"why not custom",customa)
           <button type="submit"> Submit </button>
         </form>
       </div>
-        <hr/>
-<div>     
+      <hr />
+      <div>
+        show custom target and allow to update status <br />
+        <label> selcet custom target </label> <br />
+        <select
+          onChange={(e) => {
+            customTarget(e);
+          }}
+        >
+          <option> ...choose....</option>
+          <option value="Custom"> custom </option>
+          <option value="Monthly"> Monthly </option>
+          <option value="Yearly"> yearly </option>
+        </select>
+        <select
+          onChange={(e) => {
+            statusHandle(e);
+          }}
+        >
+          <option> ...choose....</option>
+          {type.map((ty, i) => (
+            <option key={i} value={ty.customName}>
+              {" "}
+              {ty.customName}{" "}
+            </option>
+          ))}
+        </select>
+        <div style={{ backgroundColor: "yellow" }}>
+          <h3> SHOW HERE TARGET AND IT'S PROGRESS STATUS</h3>
+        </div>
+        <h4> target name{currentTarget}</h4>
+        <table>
+          <thead>
+            <tr>
+              <th> date </th>
+              <th> progress </th>
+            </tr>
+          </thead>
+          <tbody>
+            {status.map((ta, i) => (
+              <>
+                <tr key={i}>
+                  <td> {ta.date} </td>
+                  <td> {ta.status} </td>
+                </tr>
+              </>
+            ))}
+          </tbody>
+        </table>
+        <hr />
+        <form onSubmit={(e) => uploadStatusHandler(e)}>
+          <label> date: </label>
+          <input
+            type="date"
+            onChange={(e) => {
+              setUploadStatus({ ...uploadStatus, date: e.target.value });
+            }}
+          />
+          <br />
+          <label> status: </label>
+          <input
+            type="text"
+            onChange={(e) => {
+              setUploadStatus({ ...uploadStatus, status: e.target.value });
+            }}
+          />
+          <br />
 
-show custom  target  and allow to  update status <br/>
-<label> selcet custom target </label> <br/>
-
-<select onChange={(e)=> {customTarget(e)}}>      
-<option>  ...choose....</option>
-<option value= "Custom">  custom  </option>
-<option value="Monthly" >  Monthly  </option>
-<option value="Yearly" >  yearly  </option>
-</select>
-
- 
-
-<select onChange={(e)=>{ statusHandle(e)}}  >      
-<option>  ...choose....</option>
-{ type.map( (ty,i)=>    
-<option key ={i} value={ty.customName} >  {ty.customName}  </option>
-)}
-</select>
-<div style={{backgroundColor:"yellow"}}>  
-<h3> SHOW HERE TARGET AND IT'S PROGRESS STATUS</h3>
-</div>
-
-     <h4> target name{currentTarget}</h4>
-<table> 
-  <thead> 
-  <tr>  
-     <th>  date  </th>  
-  <th>    progress  </th>
-   </tr>
-   </thead>
-   <tbody>   
-
-  
-{   status.map( (ta,i)=>  
-  <>  
-  <tr key={i}>   
-<td >  {ta.date}   </td>
-<td >  {ta.status}   </td>
-</tr> 
-</>
-  ) }
-
-   </tbody>
-</table>
-<hr/>
-<form onSubmit={(e)=> uploadStatusHandler(e)}>
-<label> date: </label> 
-<input type="date" onChange={(e)=>{setUploadStatus({...uploadStatus, date:e.target.value})}} /> 
-<br/>
-<label> status: </label> 
-<input type="text" onChange={(e)=>{setUploadStatus({...uploadStatus, status:e.target.value})}}  /> 
-<br/>
-
-<button type="submit">  update status </button>
-</form>
-
-
-</div>
-
-
-
-
+          <button type="submit"> update status </button>
+        </form>
+      </div>
     </>
   );
 }
